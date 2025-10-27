831a2e40ff4d729cda1b09f563f2666591dd1647
alekseevgr <alekseevgr2@gmail.com>
2025-10-27 23:11:06 +0500

    add persist, add localstorage

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './store/store'
+import {persistor} from './store/store'
+import store from './store/store'
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './App.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 )
