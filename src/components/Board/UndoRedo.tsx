4d86229422356f7f070993d588bd0925118b11f6
alekseevgr <alekseevgr2@gmail.com>
2025-11-03 18:58:49 +0500

    disabled undo redo  in finish game

--- src/components/Board/UndoRedo.tsx
+++ src/components/Board/UndoRedo.tsx
@@ -4,14 +4,16 @@ import styles from './board.module.css';
 
 export function UndoRedo() {
   const dispatch = useAppDispatch();
-  const { prev = [], next = [] } = useAppSelector(state => state.game || {});
+  const { prev = [], next = [], winner } = useAppSelector(state => state.game || {});
+
+  const isGameOver = !!winner; 
 
   return (
     <div className={styles.undoRedoContainer}>
-      <button className={styles.undoRedoButton} onClick={() => dispatch(undo())} disabled={prev.length === 0}>
+      <button className={styles.undoRedoButton} onClick={() => dispatch(undo())} disabled={prev.length === 0 || isGameOver}>
         Отменить ход
       </button>
-      <button className={styles.undoRedoButton} onClick={() => dispatch(redo())} disabled={next.length === 0}>
+      <button className={styles.undoRedoButton} onClick={() => dispatch(redo())} disabled={next.length === 0 || isGameOver}>
         Ход вперед
       </button>
     </div>
